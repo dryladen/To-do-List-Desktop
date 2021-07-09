9ea4d1c3f7d68e10ecb6e89d0870f5b0784f0730
dryladen <delfanladen24@gmail.com>
2021-07-09 15:10:34 +0800

    Update Tampilan Menu Awal

--- src/Frame/MainFrame.java
+++ src/Frame/MainFrame.java
@@ -25,25 +25,19 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        backgroundPanel = new javax.swing.JPanel();
-        mainPanel = new javax.swing.JPanel();
         homePanel = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         pnlKategori = new javax.swing.JList<>();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        pnlTanggal = new javax.swing.JTextPane();
         jScrollPane2 = new javax.swing.JScrollPane();
-        pnlDetail = new javax.swing.JTextArea();
+        pnlDeskripsi = new javax.swing.JTextArea();
         btnTambahKategori = new Frame.CButton();
         btnUbahKategori = new Frame.CButton();
         btnHapusKategori = new Frame.CButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("To-do List");
-
-        backgroundPanel.setBackground(new java.awt.Color(51, 255, 204));
-        backgroundPanel.setBorder(javax.swing.BorderFactory.createEtchedBorder(null, new java.awt.Color(0, 204, 204)));
-
-        mainPanel.setBackground(new java.awt.Color(51, 255, 204));
-        mainPanel.setLayout(new javax.swing.BoxLayout(mainPanel, javax.swing.BoxLayout.LINE_AXIS));
 
         homePanel.setBackground(new java.awt.Color(51, 255, 204));
 
@@ -54,24 +48,36 @@ public class MainFrame extends javax.swing.JFrame {
                 pnlKategoriMouseClicked(evt);
             }
         });
+        pnlKategori.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                pnlKategoriKeyTyped(evt);
+            }
+        });
         jScrollPane1.setViewportView(pnlKategori);
 
-        pnlDetail.setEditable(false);
-        pnlDetail.setColumns(20);
-        pnlDetail.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        pnlDetail.setLineWrap(true);
-        pnlDetail.setRows(5);
-        pnlDetail.setText("Tanggal : \n-\n\nDeskripsi :\n-\n"); // NOI18N
-        pnlDetail.setAutoscrolls(false);
-        pnlDetail.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Detail", javax.swing.border.TitledBorder.LEFT, javax.swing.border.TitledBorder.ABOVE_TOP, new java.awt.Font("Tahoma", 1, 14))); // NOI18N
-        pnlDetail.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        pnlDetail.setSelectionColor(new java.awt.Color(51, 255, 51));
-        jScrollPane2.setViewportView(pnlDetail);
+        pnlTanggal.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Tanggal", javax.swing.border.TitledBorder.LEFT, javax.swing.border.TitledBorder.ABOVE_TOP, new java.awt.Font("Tahoma", 1, 13))); // NOI18N
+        jScrollPane3.setViewportView(pnlTanggal);
+
+        pnlDeskripsi.setEditable(false);
+        pnlDeskripsi.setColumns(5);
+        pnlDeskripsi.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        pnlDeskripsi.setLineWrap(true);
+        pnlDeskripsi.setRows(2);
+        pnlDeskripsi.setAutoscrolls(false);
+        pnlDeskripsi.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Detail", javax.swing.border.TitledBorder.LEFT, javax.swing.border.TitledBorder.ABOVE_TOP, new java.awt.Font("Tahoma", 1, 13))); // NOI18N
+        pnlDeskripsi.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        pnlDeskripsi.setFocusable(false);
+        pnlDeskripsi.setRequestFocusEnabled(false);
+        pnlDeskripsi.setSelectionColor(new java.awt.Color(51, 255, 51));
+        jScrollPane2.setViewportView(pnlDeskripsi);
 
         btnTambahKategori.setBackground(new java.awt.Color(204, 255, 255));
         btnTambahKategori.setText("Kategori Baru");
         btnTambahKategori.setBorderColor(new java.awt.Color(153, 153, 153));
         btnTambahKategori.setColorClick(new java.awt.Color(102, 153, 255));
+        btnTambahKategori.setMaximumSize(new java.awt.Dimension(105, 23));
+        btnTambahKategori.setMinimumSize(new java.awt.Dimension(105, 23));
+        btnTambahKategori.setPreferredSize(new java.awt.Dimension(105, 23));
         btnTambahKategori.setRadius(10);
         btnTambahKategori.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -80,9 +86,12 @@ public class MainFrame extends javax.swing.JFrame {
         });
 
         btnUbahKategori.setBackground(new java.awt.Color(204, 255, 255));
-        btnUbahKategori.setText("Ubah Kategori");
+        btnUbahKategori.setText("Ubah");
         btnUbahKategori.setBorderColor(new java.awt.Color(153, 153, 153));
         btnUbahKategori.setColorClick(new java.awt.Color(102, 153, 255));
+        btnUbahKategori.setMaximumSize(new java.awt.Dimension(105, 23));
+        btnUbahKategori.setMinimumSize(new java.awt.Dimension(105, 23));
+        btnUbahKategori.setPreferredSize(new java.awt.Dimension(105, 23));
         btnUbahKategori.setRadius(10);
         btnUbahKategori.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -91,9 +100,12 @@ public class MainFrame extends javax.swing.JFrame {
         });
 
         btnHapusKategori.setBackground(new java.awt.Color(204, 255, 255));
-        btnHapusKategori.setText("Hapus Kategori");
+        btnHapusKategori.setText("Hapus");
         btnHapusKategori.setBorderColor(new java.awt.Color(153, 153, 153));
         btnHapusKategori.setColorClick(new java.awt.Color(102, 153, 255));
+        btnHapusKategori.setMaximumSize(new java.awt.Dimension(105, 23));
+        btnHapusKategori.setMinimumSize(new java.awt.Dimension(105, 23));
+        btnHapusKategori.setPreferredSize(new java.awt.Dimension(105, 23));
         btnHapusKategori.setRadius(10);
         btnHapusKategori.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -106,63 +118,50 @@ public class MainFrame extends javax.swing.JFrame {
         homePanelLayout.setHorizontalGroup(
             homePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(homePanelLayout.createSequentialGroup()
-                .addComponent(btnTambahKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnUbahKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnHapusKategori, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addComponent(jScrollPane2)
-            .addComponent(jScrollPane1)
+                .addContainerGap()
+                .addGroup(homePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, homePanelLayout.createSequentialGroup()
+                        .addComponent(btnTambahKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnUbahKategori, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnHapusKategori, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1))
+                .addContainerGap())
         );
         homePanelLayout.setVerticalGroup(
             homePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(homePanelLayout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 202, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(homePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnTambahKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnUbahKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnHapusKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(btnHapusKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-
-        mainPanel.add(homePanel);
-
-        backgroundPanel.add(mainPanel);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(backgroundPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(homePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(backgroundPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 387, Short.MAX_VALUE)
+            .addComponent(homePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnHapusKategoriActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHapusKategoriActionPerformed
-        if(pnlKategori.isSelectionEmpty()){
-            JOptionPane.showMessageDialog(null, "Pilih kategori dulu");
-        } else {
-            try {
-                String index = dataIdKategori.get(pnlKategori.getSelectedIndex());
-                String sql = "DELETE FROM kategoriTable WHERE idKategori=?";
-                Connection cn = koneksi.getKoneksi();
-                PreparedStatement pst = cn.prepareStatement(sql);
-                pst.setString(1, index);
-                pst.execute();
-                getData();
-            } catch (SQLException ex) {
-                JOptionPane.showMessageDialog(null, "Gagal membuat kategori baru : "+ex);
-            }
-        }
-    }//GEN-LAST:event_btnHapusKategoriActionPerformed
 
     private void btnUbahKategoriActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUbahKategoriActionPerformed
         if(pnlKategori.isSelectionEmpty()){
@@ -190,7 +189,7 @@ public class MainFrame extends javax.swing.JFrame {
                 pst.setString(1, dataIdKategori.get(index));
                 ResultSet result = pst.executeQuery();
                 String tanggal = null,deskripsi=null;
-                pnlDetail.setText("");
+                pnlDeskripsi.setText("");
                 while(result.next()){
                     tanggal = result.getString(3);
                     deskripsi = result.getString(4);
@@ -201,14 +200,40 @@ public class MainFrame extends javax.swing.JFrame {
                 if(deskripsi == null || deskripsi.equals("")){
                     deskripsi = "-";
                 }
-                String detail = "Tanggal : \n"+ tanggal+ "\n\nDeskripsi : \n"+ deskripsi;
-                pnlDetail.setText(detail);
+                pnlTanggal.setText(tanggal);
+                pnlDeskripsi.setText(deskripsi);
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, ex);
                 Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }//GEN-LAST:event_pnlKategoriMouseClicked
+
+    private void pnlKategoriKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_pnlKategoriKeyTyped
+        if(!pnlKategori.isSelectionEmpty()){
+            TaskFrame task = new TaskFrame();
+            task.setVisible(true);
+            this.dispose();
+        }
+    }//GEN-LAST:event_pnlKategoriKeyTyped
+
+    private void btnHapusKategoriActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHapusKategoriActionPerformed
+        if(pnlKategori.isSelectionEmpty()){
+            JOptionPane.showMessageDialog(null, "Pilih kategori dulu");
+        } else {
+            try {
+                String index = dataIdKategori.get(pnlKategori.getSelectedIndex());
+                String sql = "DELETE FROM kategoriTable WHERE idKategori=?";
+                Connection cn = koneksi.getKoneksi();
+                PreparedStatement pst = cn.prepareStatement(sql);
+                pst.setString(1, index);
+                pst.execute();
+                getData();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Gagal membuat kategori baru : "+ex);
+            }
+        }
+    }//GEN-LAST:event_btnHapusKategoriActionPerformed
 
     /**
      * @param args the command line arguments
@@ -244,16 +269,16 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel backgroundPanel;
     private Frame.CButton btnHapusKategori;
     private Frame.CButton btnTambahKategori;
     private Frame.CButton btnUbahKategori;
     public static javax.swing.JPanel homePanel;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JPanel mainPanel;
-    private javax.swing.JTextArea pnlDetail;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTextArea pnlDeskripsi;
     private javax.swing.JList<String> pnlKategori;
+    private javax.swing.JTextPane pnlTanggal;
     // End of variables declaration//GEN-END:variables
 
     private void getData(){
